5e0ad761ebed19d9056a425f27c8e1cd17732dd8
Sertan Senturk <contact@sertansenturk.com>
2016-01-25 00:42:42 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from setuptools import setup
 
 setup(name='seyiranalyzer',
       version='0.1',
-      description='Tools to analyse the seyir (melodic progression) of scores and audio recordings of makam music',
       author='Sertan Senturk',
+      author_email='contact AT sertansenturk DOT com',
+      license='agpl 3.0',
+      description='Tools to analyse the seyir (melodic progression) of scores and audio recordings of makam music',
       url='http://sertansenturk.com',
       packages=['seyiranalyzer'],
 )
