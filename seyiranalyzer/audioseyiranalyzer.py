84cdf04a13d5599dc28cf4631d56c15137b0958c
Sertan Senturk <contact@sertansenturk.com>
2016-04-19 12:06:46 +0200

    Renamed module according PEP8 convention

--- seyiranalyzer/audioseyiranalyzer.py
+++ seyiranalyzer/audioseyiranalyzer.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-from modetonicestimation.PitchDistribution import PitchDistribution
-from modetonicestimation.Converter import Converter
+from morty.PitchDistribution import PitchDistribution
+from morty.Converter import Converter
 
 
 class AudioSeyirAnalyzer(object):
