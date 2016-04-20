8e38b7cca28fb63469f4b782add69038598d82bc
Sertan Senturk <contact@sertansenturk.com>
2016-04-20 14:44:01 +0200

    Updated morty version

--- seyiranalyzer/audioseyiranalyzer.py
+++ seyiranalyzer/audioseyiranalyzer.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-from morty.PitchDistribution import PitchDistribution
-from morty.Converter import Converter
+from morty.pitchdistribution import PitchDistribution
+from morty.converter import Converter
 
 
 class AudioSeyirAnalyzer(object):
